6bd64905ff8ee2d9a6d8dd17ceab082448c5471b
crispynoodlesoup <thepanman321@gmail.com>
2023-11-02 15:58:00 -0500

    create a basic nav and main structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import style from "./style.module.css";
 function App() {
   let backgroundLength = 9;
   let backgroundArray = [];
-  for(let i = 0; i < backgroundLength; i++)
-    backgroundArray.push(i);
+  for (let i = 0; i < backgroundLength; i++) backgroundArray.push(i);
 
   return (
-    <div className={style.home}>
+    <div className={style.pageContainer}>
+      <div className={style.home}>
+        <nav>
+          <ul>
+            <li>HOME</li>
+            <li>SHOP</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+          </ul>
+        </nav>
+        <main>
+          <div className={style.contentBubble}>
+            <h1>MABU - MABU</h1>
+          </div>
+        </main>
+      </div>
       <div className={style.background}>
-        {backgroundArray.map((panelNum) => <div key={panelNum}></div>)}
+        {backgroundArray.map((panelNum) => (
+          <div key={panelNum}></div>
+        ))}
       </div>
     </div>
   );
