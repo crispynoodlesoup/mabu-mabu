1891d53472885b9ae8b7b06eec5f1d2b06e4e929
crispynoodlesoup <thepanman321@gmail.com>
2023-11-02 15:48:36 -0500

    create an infinite scrolling background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import style from "./style.module.css";
+
 function App() {
+  let backgroundLength = 9;
+  let backgroundArray = [];
+  for(let i = 0; i < backgroundLength; i++)
+    backgroundArray.push(i);
+
   return (
-    <>
-      <p>hello world!</p>
-    </>
-  )
+    <div className={style.home}>
+      <div className={style.background}>
+        {backgroundArray.map((panelNum) => <div key={panelNum}></div>)}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
