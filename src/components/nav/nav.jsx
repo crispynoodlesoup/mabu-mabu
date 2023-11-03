2643c2527874fe9b61d6528e90a0bd67017fabed
crispynoodlesoup <thepanman321@gmail.com>
2023-11-03 02:01:40 -0500

    create selected style in nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,5 +1,5 @@
-// eslint-disable-next-line
 import navStyle from "./nav.module.css";
+// eslint-disable-next-line
 import homeButton from "../../assets/home.svg";
 import shopButton from "../../assets/shop.svg";
 import aboutButton from "../../assets/about.svg";
@@ -9,7 +9,9 @@ function Nav() {
   return (
     <nav>
       <ul>
-        <li className={navStyle.navLink}><img src={homeButton} alt="" /></li>
+        <li className={`${navStyle.navLink} ${navStyle.selected}`}>
+            HOME
+        </li>
         <li className={navStyle.navLink}><img src={shopButton} alt="" /></li>
         <li className={navStyle.navLink}><img src={aboutButton} alt="" /></li>
         <li className={navStyle.navLink}><img src={contactButton} alt="" /></li>
