5107f65a586b1444f058721c9587b5680860e413
crispynoodlesoup <thepanman321@gmail.com>
2023-11-03 00:58:06 -0500

    make fancy nav buttons

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,14 +1,18 @@
 // eslint-disable-next-line
 import navStyle from "./nav.module.css";
+import homeButton from "../../assets/home.svg";
+import shopButton from "../../assets/shop.svg";
+import aboutButton from "../../assets/about.svg";
+import contactButton from "../../assets/contact.svg";
 
 function Nav() {
   return (
     <nav>
       <ul>
-        <li className={navStyle.navLink}>HOME</li>
-        <li className={navStyle.navLink}>SHOP</li>
-        <li className={navStyle.navLink}>ABOUT</li>
-        <li className={navStyle.navLink}>CONTACT</li>
+        <li className={navStyle.navLink}><img src={homeButton} alt="" /></li>
+        <li className={navStyle.navLink}><img src={shopButton} alt="" /></li>
+        <li className={navStyle.navLink}><img src={aboutButton} alt="" /></li>
+        <li className={navStyle.navLink}><img src={contactButton} alt="" /></li>
       </ul>
     </nav>
   );
