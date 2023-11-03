0ecc28f808d39f5dc0f675555ce4bea780e8815b
crispynoodlesoup <thepanman321@gmail.com>
2023-11-03 02:15:56 -0500

    add ice cream.

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,17 +4,23 @@ import homeButton from "../../assets/home.svg";
 import shopButton from "../../assets/shop.svg";
 import aboutButton from "../../assets/about.svg";
 import contactButton from "../../assets/contact.svg";
+import iceCream from "../../assets/ice-cream.svg"
 
 function Nav() {
   return (
     <nav>
       <ul>
-        <li className={`${navStyle.navLink} ${navStyle.selected}`}>
-            HOME
+        <li className={`${navStyle.navLink} ${navStyle.selected}`}>HOME</li>
+        <li className={navStyle.navLink}>
+          <img src={shopButton} alt="" />
         </li>
-        <li className={navStyle.navLink}><img src={shopButton} alt="" /></li>
-        <li className={navStyle.navLink}><img src={aboutButton} alt="" /></li>
-        <li className={navStyle.navLink}><img src={contactButton} alt="" /></li>
+        <img className={navStyle.iceCream} src={iceCream} alt="" />
+        <li className={navStyle.navLink}>
+          <img src={aboutButton} alt="" />
+        </li>
+        <li className={navStyle.navLink}>
+          <img src={contactButton} alt="" />
+        </li>
       </ul>
     </nav>
   );
