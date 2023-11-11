7c7cb25c383c98627226cb85a247b1e1a8bfdb70
crispynoodlesoup <thepanman321@gmail.com>
2023-11-11 17:44:09 -0600

    implement bubble component in home

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,21 +1,21 @@
 import Nav from "../nav/nav.jsx";
+// eslint-disable-next-line
 import style from "../../style.module.css";
 import homeStyle from "./home.module.css";
+import ContentBubble from "../contentBubble/ContentBubble.jsx";
 
 function Home() {
   return (
     <div className={homeStyle.home}>
       <Nav></Nav>
       <main>
-        <div className={`${style.contentBubble} ${homeStyle.titleBubble}`}>
-          <div className={style.contentWrapper}>
-            <div className={homeStyle.titleStripe}>
-              <div></div>
-              <div></div>
-            </div>
-            <h1 className={homeStyle.heroTitle}>MABU - MABU</h1>
+        <ContentBubble>
+          <div className={homeStyle.titleStripe}>
+            <div></div>
+            <div></div>
           </div>
-        </div>
+          <h1 className={homeStyle.heroTitle}>MABU - MABU</h1>
+        </ContentBubble>
       </main>
     </div>
   );
