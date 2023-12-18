5efc7bd409329db40c9c7edcd5aab1cf0693b09f
crispynoodlesoup <thepanman321@gmail.com>
2023-12-18 15:42:42 -0600

    complete raising pageContent for rest of pages

--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -1,14 +1,12 @@
-import Nav from "../nav/nav.jsx";
-import shopStyle from "./shop.module.css";
+// import shopStyle from "./shop.module.css";
 // import ContentBubble from "../contentBubble/ContentBubble.jsx";
 import PageHeader from "../pageHeader/PageHeader.jsx";
 
 function Shop() {
   return (
-    <div className={shopStyle.shop}>
-      <Nav/>
+    <>
       <PageHeader text={"THE SHOP"}/>
-    </div>
+    </>
   );
 }
 
