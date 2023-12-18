6b8c4cc1b72a536fa3bd5d2996e0752965db745b
crispynoodlesoup <thepanman321@gmail.com>
2023-12-18 15:56:04 -0600

    add main bubble to shop page

--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -1,11 +1,19 @@
-// import shopStyle from "./shop.module.css";
-// import ContentBubble from "../contentBubble/ContentBubble.jsx";
+import shopStyle from "./shop.module.css";
+import ContentBubble from "../contentBubble/ContentBubble.jsx";
 import PageHeader from "../pageHeader/PageHeader.jsx";
 
 function Shop() {
   return (
     <>
-      <PageHeader text={"THE SHOP"}/>
+      <PageHeader text={"THE SHOP"} />
+      <main className={shopStyle.gridWrapper}>
+        <ContentBubble>
+            <h2>Explore Our Selection!</h2>
+            <p>
+            Check out our awesome dessert selection below, and order as many as you want!
+            </p>
+        </ContentBubble>
+      </main>
     </>
   );
 }
